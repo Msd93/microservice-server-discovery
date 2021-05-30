ddc72cb336b6ea81ad7026cf8235587f2ad1a2f2
Mamadou Seydou Diallo <diallodiery301@gmail.com>
2021-05-30 00:26:47 +0000

    boostrap project

---
+++ src/main/java/sn/pad/discovery/PadServerDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package sn.pad.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class PadServerDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadServerDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/pad/discovery/PadServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+//package sn.pad.discovery;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PadServerDiscoveryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
